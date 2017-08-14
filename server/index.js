e740d35b67507e5b6b86446f3ee57e0553d1fb21
3hdeng <3hdeng@gmail.com>
2017-08-14 15:26:27 +0800

    express app route serving depends on the order appearing in the source code index.js; app.get('*', ) has to be placed as the last route?

--- server/index.js
+++ server/index.js
@@ -10,11 +10,50 @@ app.use(express.static('static'));
 
 app.use('/public', express.static(path.join(__dirname, '../public')));
 app.use('/api', routes);
+
+//=== swagger spec =====================
+var swaggerJSDoc = require('swagger-jsdoc');
+
+// swagger definition
+var swaggerDefinition = {
+  info: {
+    title: 'Node Swagger API',
+    version: '1.0.0',
+    description: 'Demonstrating how to describe a RESTful API with Swagger',
+  },
+  host: 'localhost:'+ port,
+  basePath: '/',
+};
+
+// options for the swagger docs
+var options = {
+  // import swaggerDefinitions
+  swaggerDefinition: swaggerDefinition,
+  // path to the API docs
+  apis: ['./routes/*.js'],
+};
+
+// initialize swagger-jsdoc
+var swaggerSpec = swaggerJSDoc(options);
+console.log(swaggerSpec);
+//========================
+
+// serve swagger
+app.get('/swagger.json', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+
+
 app.get('*', function (request, response){ response.sendFile(path.resolve(__dirname, '../', 'index.html')) });
 
+//=======================
 const server = app.listen(port, () => {
   const port = server.address().port;
   console.log('nodegit server running on ', port);
 });
 
+
+
 module.exports = server;
+

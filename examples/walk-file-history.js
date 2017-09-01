57ee54bca1b8d58f45ffe4b69691c8b37899a4e6
3hdeng <3hdeng@gmail.com>
2017-09-01 17:27:18 +0800

    by comparing the last two entries shaid to detect the end; having problem when only one entry is accumulated in the history array

--- examples/walk-file-history.js
+++ examples/walk-file-history.js
@@ -7,75 +7,84 @@ console.log("noteDir resolved to " + dirName);
 
 
 var Git = require("nodegit"),
-    historyFile = "cat0/cat0_f1.txt", //"/workspaces/mynodegit/examples/file_history.json",
+    historyFile = "cat33/f33.txt",
     walker,
     historyCommits = [],
     commit,
     repo;
 //===============
+var _walker;
+var _itercount=10;
+const _maxcount = 1000;
 
+//=== recursive call self - compileHistory ?
 
 function compileHistory(resultingArrayOfCommits) {
-  var lastSha;
-  if (historyCommits.length > 0) {
-    lastSha = historyCommits[historyCommits.length - 1].commit.sha();
-    if (
-      resultingArrayOfCommits.length == 1 &&
-      resultingArrayOfCommits[0].commit.sha() == lastSha
-    ) {
-      return;
-    }
-  }
+	var lastSha;
+	if (historyCommits.length > 0) {
+		lastSha = historyCommits[historyCommits.length - 1].commit.sha();
+		if (
+				resultingArrayOfCommits.length == 1 &&
+				resultingArrayOfCommits[0].commit.sha() == lastSha
+		   ) {
+			return;
+		}
+	}
 
-  resultingArrayOfCommits.forEach(function(entry) {
-    historyCommits.push(entry);
-  });
-   
-  if(historyCommits.length <=0 ) {
-    console.log("no commit found!!");
-    return;
-  }
+	resultingArrayOfCommits.forEach(function(entry) {
+			historyCommits.push(entry);
+			});
 
-  lastSha = historyCommits[historyCommits.length - 1].commit.sha();
+	if(historyCommits.length <=0 ) {
+		console.log("no commit matching the file found yet!!");
+		_itercount = _itercount*2;
+		_itercount= (_itercount > _maxcount ? _maxcount: _itercount); 
+                console.log("_itercount increased to " + _itercount);
 
-  walker = repo.createRevWalk();
-  walker.push(lastSha);
-  walker.sorting(Git.Revwalk.SORT.TIME);
+		return _walker.fileHistoryWalk(historyFile, _itercount).then(compileHistory);
+	} else {
+		lastSha = historyCommits[historyCommits.length - 1].commit.sha();
+		_walker.free();
 
-  return walker.fileHistoryWalk(historyFile, 100)
-    .then(compileHistory);
+		walker = repo.createRevWalk();
+		walker.push(lastSha);
+		walker.sorting(Git.Revwalk.SORT.TIME);
+		_walker=walker;
+		return walker.fileHistoryWalk(historyFile, _itercount)
+			.then(compileHistory);
+	}
 }
 
 
 
-//=================
+//================
 Git.Repository.open(dirName)
-  .then(function(r) {
-    repo = r;
-    return repo.getMasterCommit();
-  })
-  .then(function(firstCommitOnMaster){
-    // History returns an event.
-    walker = repo.createRevWalk();
-    walker.push(firstCommitOnMaster.sha());
-    walker.sorting(Git.Revwalk.SORT.Time);
-    console.log("firstCommitOnMaster.sha():" + firstCommitOnMaster.sha() );
-
-    return walker.fileHistoryWalk(historyFile, 100).then(compileHistory)
-})
-  .then(function() {
-	console.log("walker.fileHistoryWalk() finished ? or not ?");
-    historyCommits.forEach(function(entry) {
-      commit = entry.commit;
-      //console.log("entry.path(), " + entry.path());
-      console.log("commit " + commit.sha());
-      console.log("Author:", commit.author().name() +
-        " <" + commit.author().email() + ">");
-      console.log("Date:", commit.date());
-      console.log("\n    " + commit.message());
-    });
-  })
-  .catch( err=> {console.log(err);} )
-  .done(()=>{console.log("done");}) ;
+	.then(function(r) {
+			repo = r;
+			return repo.getMasterCommit();
+			})
+.then(function(firstCommitOnMaster){
+		// History returns an event.
+		walker = repo.createRevWalk();
+		walker.push(firstCommitOnMaster.sha());
+		walker.sorting(Git.Revwalk.SORT.Time);
+		console.log("firstCommitOnMaster.sha():" + firstCommitOnMaster.sha() );
+		_walker=walker;
+		return walker.fileHistoryWalk(historyFile, _itercount).then(compileHistory);
+		})
+.then(function() {
+		console.log("walker.fileHistoryWalk() finished ? or not ?");
+		historyCommits.forEach(function(entry) {
+			commit = entry.commit;
+			//console.log("entry.path(), " + entry.path());
+			console.log("commit " + commit.sha());
+			console.log("Author:", commit.author().name() +
+				" <" + commit.author().email() + ">");
+			console.log("Date:", commit.date());
+			console.log("\n    " + commit.message());
+			});
+		})
+.catch( err=> {console.log(err);} )
+.done(()=>{console.log("done");}) ;
 
 
